a246d0800a2de9c21b579333a36fe0dc320171b1
vjodo <senchikhin@gmail.com>
2024-03-20 15:46:38 +0300

    Уровень 7. Функции

---
+++ src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0701;
+
+/*
+Все возможные типы
+*/
+
+public class Solution {
+    byte i;
+    short x;
+    int y;
+    long z;
+    float q;
+    double w;
+    boolean e;
+    char k;
+}
