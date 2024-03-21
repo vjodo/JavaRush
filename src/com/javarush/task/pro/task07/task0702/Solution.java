d3ad8c9b0f0e76b0e3334c111937136d89181474
vjodo <senchikhin@gmail.com>
2024-03-21 21:02:11 +0300

    Уровень 8. Типы данных

---
+++ src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0702;
+
+/*
+Максимальное целое
+*/
+
+public class Solution {
+    byte i = Byte.MAX_VALUE;
+    short x = Short.MAX_VALUE;
+    int y = Integer.MAX_VALUE;
+    long z = Long.MAX_VALUE;
+
+}
---
+++ src/com/javarush/task/pro/task07/task0703/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task07.task0703;
+
+/* 
+Минимальное целое
+*/
+
+public class Solution {
+    byte i = Byte.MIN_VALUE;
+    short x = Short.MIN_VALUE;
+    int y = Integer.MIN_VALUE;
+    long z = Long.MIN_VALUE;
+}
