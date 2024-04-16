a80057cc4e42b22d6a5f6eb521ec7e3cf02912f9
vjodo <senchikhin@gmail.com>
2024-04-17 00:11:13 +0300

    Уровень 13. Коллекции, Generics

---
+++ src/com/javarush/task/pro/task13/task1304/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.pro.task13.task1304;
+
+import java.util.*;
+
+/*
+Переводим итератор в цикл for-each
+*/
+
+public class Solution {
+
+    public static void printList(ArrayList<String> words) {
+        for (String s : words)
+            System.out.println(s);
+    }
+
+    public static void printHashSet(HashSet<String> words) {
+        for (String s : words)
+            System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+        String[] wordsArray = "Думаю, это будет новой фичей. Только не говорите никому, что она возникла случайно.".split(" ");
+        ArrayList<String> wordsList = new ArrayList<>(Arrays.asList(wordsArray));
+        HashSet<String> wordsHashSet = new HashSet<>(wordsList);
+        printList(wordsList);
+        System.out.println("__________________________________");
+        printHashSet(wordsHashSet);
+    }
+}
